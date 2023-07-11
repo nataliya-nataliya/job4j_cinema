ef9858b55348c715dde0a4f1adeeb69676835144
nataliya-nataliya <nataliya.chest@gmail.com>
2023-07-12 02:01:55 +0400

    add liquibase and models

--- db/scripts/001_ddl_create_initial_schema.sql
+++ db/scripts/001_ddl_create_initial_schema.sql
@@ -36,7 +36,7 @@ create table film_sessions
 (
     id         serial primary key,
     film_id    int references films (id) not null,
-    halls_id   int references halls (id) not null,
+    hall_id    int references halls (id) not null,
     start_time timestamp                 not null,
     end_time   timestamp                 not null,
     price      int                       not null
--- db/scripts/002_dml_insert_schema.sql
+++ db/scripts/002_dml_insert_schema.sql
@@ -13,7 +13,7 @@ insert into films(name, description, "year", genre_id, minimal_age, duration_in_
 insert into halls(name, row_count, place_count, description) values('Hall 1', 5, 7, 'Description 1');
 insert into halls(name, row_count, place_count, description) values('Hall 2', 10, 10, 'Description 2');
 
-insert into film_sessions(film_id, halls_id, start_time, end_time, price) values(1, 1, '2023-07-10 14:00:00', '2023-07-10 15:20:00', 300);
-insert into film_sessions(film_id, halls_id, start_time, end_time, price) values(1, 2, '2023-07-12 19:00:00', '2023-07-12 20:20:00', 300);
-insert into film_sessions(film_id, halls_id, start_time, end_time, price) values(2, 1, '2023-07-10 16:00:00', '2023-07-10 17:40:00', 400);
-insert into film_sessions(film_id, halls_id, start_time, end_time, price) values(3, 2, '2023-07-14 14:00:00', '2023-07-14 15:30:00', 300);
+insert into film_sessions(film_id, hall_id, start_time, end_time, price) values(1, 1, '2023-07-10 14:00:00', '2023-07-10 15:20:00', 300);
+insert into film_sessions(film_id, hall_id, start_time, end_time, price) values(1, 2, '2023-07-12 19:00:00', '2023-07-12 20:20:00', 300);
+insert into film_sessions(film_id, hall_id, start_time, end_time, price) values(2, 1, '2023-07-10 16:00:00', '2023-07-10 17:40:00', 400);
+insert into film_sessions(film_id, hall_id, start_time, end_time, price) values(3, 2, '2023-07-14 14:00:00', '2023-07-14 15:30:00', 300);
